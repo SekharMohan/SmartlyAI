ea9da21af9ab4b4ee178559d18b4f8e00291963d
Sekhar Madhiyazhagan <sekarmohan77@gmail.com>
2017-11-11 06:04:23 +0530

    Initial commit

---
+++ app/src/main/java/com/poc/smartly/ai/MainActivity.java
@@ -0,0 +1,13 @@
+package com.poc.smartly.ai;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
